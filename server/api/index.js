b10de38570bd462f52924e18119d5b749cb0381e
githubzohaib <zohaibaliwork@gmail.com>
2026-04-27 20:17:39 +0500

    new changes

--- server/api/index.js
+++ server/api/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 // ✅ FIRST: CORS
 app.use(cors({
-  origin: "http://localhost:5174", // your frontend port
+  origin: "http://localhost:5173", // your frontend port
   methods: ["GET", "POST"],
   credentials: true
 }));
